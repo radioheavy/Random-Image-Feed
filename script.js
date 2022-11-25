82a8a63f78e6d850aab4c86c8ea644f1de0bb1da
İsmail Oktay Dak <dakoktaytgb@gmail.com>
2022-11-25 23:16:09 +0300

    s

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+//
 const open_btn = document.querySelector('.open-btn')
 const close_btn = document.querySelector('.close-btn')
 const nav = document.querySelectorAll('.nav')
@@ -9,7 +12,6 @@ for(let i = 0; i<rows *3; i++){
     const img = document.createElement('img')
     img.src = `${unsplashURL}${getRandomSize()}`
     container.appendChild(img)
-
 }
 
 function getRandomSize(){
@@ -28,4 +30,25 @@ close_btn.addEventListener('click', () => {
      nav.forEach(nav_el => nav_el.classList.remove('visible'))
  })
 
- 
+ //Load 
+
+ let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+  load++
+
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
